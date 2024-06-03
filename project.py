50fae29706ba4c2dad09a68df7c68417d128642c
shayannj84 <shayannajafian4@gmail.com>
2024-06-03 13:41:23 +0430

    Update project.py

--- project.py
+++ project.py
@@ -1,5 +1,3 @@
-from os import name
-from numpy import delete
 import pandas as pd
 
 class Book:
@@ -23,7 +21,7 @@ class Book:
 
 class Library:
     def __init__(self , lib_name: str, *args : Book):
-        self. lib_name = lib_name
+        self.lib_name = lib_name
         self.books = list(args)
         self.books_dict = {'name':[] , 'author': [],'keyword': [],'release_date': []}
         for book in self.books:
@@ -46,23 +44,33 @@ class Library:
     def delete(self, book : Book):
         df = pd.DataFrame(self.books_dict)
         for ind in range(len(df)):
-            if self.df.iloc[ind, 0] == book.name:
-                self.df.drop([ind])
-
-    def edit(self, book : Book):
-        # delete the current book
-        for ind in self.range(len(self.df)):
-            if self.df.iloc[ind, 0] == book.name:
-                self.df.drop([ind])
-                # add the new book
-                self.books_dict['name'].append(book.name)
-                self.books_dict['author'].append(book.author)
-                self.books_dict['keyword'].append(book.keyword)
-                self.books_dict['release_date'].append(Book.release_date)
-                df = pd.DataFrame(self.books_dict)
-                df.to_csv('library.csv', sep='-', index=False)  
-
+            if df.iloc[ind, 0] == book.name:
+                self.books_dict['name'].remove(book.name)
+                self.books_dict['author'].remove(book.author)
+                self.books_dict['keyword'].remove(book.keyword)
+                self.books_dict['release_date'].remove(book.release_date)
                 
+                print('The book is deleted')
+        new_df = pd.DataFrame(self.books_dict)
+        new_df.to_csv('library.csv', index=False)    
+    
+    def edit(self, oldbook : Book , newbook : Book):
+        # delete the current book
+        df = pd.DataFrame(self.books_dict)
+        for ind in range(len(df)):
+            if df.iloc[ind, 0] == newbook.name:
+                self.books_dict['name'].remove(oldbook.name)
+                self.books_dict['author'].remove(oldbook.author)
+                self.books_dict['keyword'].remove(oldbook.keyword)
+                self.books_dict['release_date'].remove(oldbook.release_date)
+        
+                # add the new book
+        self.books_dict['name'].append(newbook.name)        
+        self.books_dict['author'].append(newbook.author)
+        self.books_dict['keyword'].append(newbook.keyword)
+        self.books_dict['release_date'].append(newbook.release_date)
+        new_df = pd.DataFrame(self.books_dict)
+        new_df.to_csv('library.csv', sep='-', index=False)  
 
     def exists(self, book : Book):
          for ind in self.range(len(self.df)):
@@ -76,12 +84,12 @@ book1 = Book('On the Origin of Species', 'Charles Darwin','akf','1859')
 book2 = Book('Cosmos', 'Carl Sagan','acf','1980')
 book3 = Book('The Selfish Gene', 'Richard Dawkin','csd','1976')
 science_lib = Library('sci', book1, book2, book3)
-book4 = Book(name='Silent Spring', author='Rachel Carson',keyword='auf',release_date='1962')
-science_lib.delete(book4)
-
-
-
-
-        
-        
-        
+book4 = Book('Silent Spring', 'Rachel Carson','auf','1962')
+science_lib.delete(book2)
+science_lib.delete(book3)
+book5 = Book('The Elegant Universe', 'Brian Greene','auk','1999')
+science_lib.add(book5)
+book6 = Book('The Grand Design', 'Leonard Mlodinow','guk','2010')
+science_lib.add(book6)
+book7 = Book('The Grand Design', 'Leonard Mlodinow','guk','2016')
+science_lib.edit(book6,book7)  

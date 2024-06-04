9d9445101592fe2158f1156f59c7be60b06166cc
pouria :) <pouriapmd@gmail.com>
2024-06-04 17:00:17 +0330

    viele probleme

--- interactions.py
+++ interactions.py
@@ -1,6 +1,15 @@
-# functions in this file are commands of the buttons and used to interact with the dataframe.
+import os
 
-def add_lib_cmd():
+# functions in this file are commands of the buttons and used to interact with the dataframe.
+def add_lib_cmd(lib_name):
+    for library in os.listdir(f'{os.getcwd()}\\csv_files'):
+        if lib_name == library:
+            pass
+        else:
+            pass
+
+
+
 
 
 

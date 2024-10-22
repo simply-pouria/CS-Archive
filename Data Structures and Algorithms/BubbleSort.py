4cf09fc44b733fe688544bdba23ac86217901d5f
pouria :) <pouriapmd@gmail.com>
2024-10-22 15:28:26 +0330

    Bubble Sort Optimized

--- Data Structures and Algorithms/BubbleSort.py
+++ Data Structures and Algorithms/BubbleSort.py
@@ -15,3 +15,15 @@ bubble_sort(arr=arr)
 print(arr)
 
 
+# Early Exit Optimization for already sorted arrays
+def bubble_sort(arr:list):
+    l = len(arr)
+    for i in range(l-1):
+        swapped = False
+        for j in range(l-1, i, -1):
+            if arr[j-1] > arr[j]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+                swapped = True
+        if not swapped:
+            break  # If no two elements were swapped, the array is already sorted
+

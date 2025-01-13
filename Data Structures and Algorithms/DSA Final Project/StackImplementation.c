04dc2ea870def416411ae3c68b40ed61c683d0aa
pouria :) <pouriapmd@gmail.com>
2025-01-13 16:45:26 +0330

    structs and declarations

--- Data Structures and Algorithms/DSA Final Project/StackImplementation.c
+++ Data Structures and Algorithms/DSA Final Project/StackImplementation.c
@@ -2,17 +2,43 @@
 
 # include <stdio.h>
 
-typedef
+//structs
+typedef struct Node {
+    int* data;
+    struct Node* next;    // Pointer to next node
+    struct Node* prev;    // Pointer to previous node
+} Node;
 
+typedef struct {
+    Node* head;           // Points to the first node
+    int size;             // Number of elements in list
+    void (*free_data)(void*);  // Function to free data
+} CircularList;
 
+// operations declaration
+void insert_head(CircularList *circular_list, int insert_value);
 
+void insert_tail(CircularList *circular_list, int insert_value);
 
+void delete_node(CircularList *circular_list, int delete_value);
 
+void search(CircularList *circular_list, int search_value);
 
+void print_list(CircularList *circular_list)
 
-
-
+// driver code
 int main() {
 
     return 0;
 }
+
+// operations definition
+
+
+
+
+
+
+
+
+
